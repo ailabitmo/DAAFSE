a07bab20497bab011d051212427e9bde5619130f
Maxim Kolchin <kolchinmax@gmail.com>
2014-05-07 10:35:01 +0400

    Fixed #9. And replaced log4j to logback.

---
+++ stream-publisher/src/main/java/ru/ifmo/ailab/daafse/streampublisher/Producer.java
@@ -0,0 +1,99 @@
+package ru.ifmo.ailab.daafse.streampublisher;
+
+import com.rabbitmq.client.AlreadyClosedException;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.ShutdownListener;
+import com.rabbitmq.client.ShutdownSignalException;
+import java.io.IOException;
+import java.net.URI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Producer implements ShutdownListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(Producer.class);
+    private final URI serverUri;
+    private final String exchangeName;
+    private volatile boolean connected = false;
+    private Connection connection;
+    private Channel channel;
+
+    public Producer(final URI serverUri, final String exchangeName) {
+        this.serverUri = serverUri;
+        this.exchangeName = exchangeName;
+    }
+
+    public void init() {
+        ConnectionFactory factory = new ConnectionFactory();
+        try {
+            factory.setUri(this.serverUri);
+        } catch (Exception ex) {
+            logger.debug(ex.getMessage(), ex);
+        }
+
+        while (!connected) {
+            try {
+                connection = factory.newConnection();
+                connection.addShutdownListener(this);
+
+                channel = connection.createChannel();
+                channel.exchangeDeclare(this.exchangeName, "topic");
+
+                connected = true;
+                logger.debug("Connected to AMQP broker!");
+            } catch (IOException ex) {
+                logger.warn(
+                        "Connection failed, will try to connect in a 5 secs!");
+                connected = false;
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException ie) {
+                    logger.debug(
+                            "The thread was interrupted while reconnection timeout!");
+                }
+            }
+        }
+    }
+
+    public void publish(final String routingKey, final byte[] data) throws IOException {
+        try {
+            if(connected) {
+                channel.basicPublish(exchangeName, routingKey, null, data);
+            } else {
+                logger.warn("Not connected! Ignoring message...");
+            }
+        } catch (AlreadyClosedException ex) {
+            logger.debug(ex.getMessage(), ex);
+        }
+    }
+
+    public void close() {
+        connected = false;
+        try {
+            if (channel != null && channel.isOpen()) {
+                channel.close();
+                channel = null;
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        try {
+            if (connection != null && connection.isOpen()) {
+                connection.close();
+                connection = null;
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Override
+    public void shutdownCompleted(ShutdownSignalException cause) {
+        logger.debug("Received ShutDownSignalException. Reconnecting...");
+        close();
+        init();
+    }
+
+}
--- stream-publisher/src/main/java/ru/ifmo/ailab/daafse/streampublisher/Publisher.java
+++ stream-publisher/src/main/java/ru/ifmo/ailab/daafse/streampublisher/Publisher.java
@@ -1,38 +1,33 @@
 package ru.ifmo.ailab.daafse.streampublisher;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import org.aeonbits.owner.ConfigFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.ifmo.ailab.daafse.streampublisher.config.PublisherConfig;
 
 public class Publisher implements ObservationListener {
 
     private static final PublisherConfig config = ConfigFactory
             .create(PublisherConfig.class);
-    private static Connection connection;
-    private static Channel channel;
+    private static final Producer producer = new Producer(
+            config.serverURI(), config.exchangeName());
+    private static final Logger logger = LoggerFactory.getLogger(Publisher.class);
 
     public static void main(String[] args) throws Exception {
         try {
             Path log = Paths.get(args[0]);
+            logger.info("Observations will be read from {} file.", log);
             LogReader lr = new LogReader(log, new Publisher());
-
-            ConnectionFactory factory = new ConnectionFactory();
-            factory.setUri(config.serverURI());
-            connection = factory.newConnection();
-
-            channel = connection.createChannel();
-            channel.exchangeDeclare(config.exchangeName(), "topic");
+            producer.init();
 
             lr.run();
 
         } finally {
-            connection.close();
+            producer.close();
         }
     }
 
@@ -41,11 +36,12 @@ public class Publisher implements ObservationListener {
         try {
             ByteArrayOutputStream out = new ByteArrayOutputStream();
             observation.getModel().write(out);
-            channel.basicPublish(config.exchangeName(),
-                    config.routingKey(observation.getMeterId()),
-                    null, out.toByteArray());
+            producer.publish(config.routingKey(observation.getMeterId()),
+                    out.toByteArray());
+            logger.debug("Published to {}", 
+                    config.routingKey(observation.getMeterId()));
         } catch (IOException ex) {
-            ex.printStackTrace();
+            logger.debug(ex.getMessage(), ex);
         }
     }
 
