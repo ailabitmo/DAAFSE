44eeb5c1833a652aab7fc05bc5c7321bbcbd8199
Maxim Kolchin <kolchinmax@gmail.com>
2014-08-14 22:10:07 +0400

    Renamed the war to ROOT.war

--- dockerfiles/alert-service/deploy.sh
+++ dockerfiles/alert-service/deploy.sh
@@ -3,29 +3,32 @@ JBOSS_DEPS=$JBOSS_HOME/standalone/deployments
 
 APP_DIR=/home/wildfly/DAAFSE/alert-service
 APP_NAME=alert-service-1.0-SNAPSHOT
-WAR_NAME=$APP_NAME.war
+SOURCE_WAR_NAME=$APP_NAME.war
+DEST_WAR_NAME=ROOT.war
 
 echo "JBOSS_HOME=${JBOSS_HOME}"
 echo "JBOSS_DEPS=${JBOSS_DEPS}"
+echo "APP_DIR=${APP_DIR}"
 echo "APP_NAME=${APP_NAME}"
-echo "WAR_NAME=${WAR_NAME}"
+echo "SOURCE_WAR_NAME=${SOURCE_WAR_NAME}"
+echo "DEST_WAR_NAME=${DEST_WAR_NAME}"
 
 if [ "$1" == "redeploy" ] ; then
 	#Cleanup
-	rm -rf $JBOSS_DEPS/$APP_NAME*
+	rm -rf $JBOSS_DEPS/$DEST_WAR_NAME*
 	rm -rf $JBOSS_HOME/cqels_home
 
 	#Build and unzip
 	pushd $APP_DIR
-	
+
 	#Update the local repository
 	git pull
 
 	mvn clean package -DskipTests=true
-	unzip target/$WAR_NAME -d $JBOSS_DEPS/$WAR_NAME
+	unzip target/$SOURCE_WAR_NAME -d $JBOSS_DEPS/$DEST_WAR_NAME
 	popd
 	
-	touch $JBOSS_DEPS/$WAR_NAME.dodeploy
+	touch $JBOSS_DEPS/$DEST_WAR_NAME.dodeploy
 elif [ "$1" == "run" ] ; then
 	/opt/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
 fi
